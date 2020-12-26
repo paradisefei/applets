bc5949093ef6fa68814be21d3445bbffc9eacb48
paradisefei <1113693141@qq.com>
2020-12-26 16:24:07 +0800

    add: 登录成功&个人中心渲染

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,4 @@
+import ajax from "../../utils/ajax.js";
 // pages/login/login.js
 /**
  * 1.给游客区域绑定点击事件
@@ -8,6 +9,8 @@
  *    4.将两次收集放到一个函数中去处理
  * 3.小程序进行数据绑定
  *    1.model:前缀
+ * 4.收集到数据发送请求
+ *    1.小程序表单校验
  */
 Page({
 
@@ -15,8 +18,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    phone: "",
-    password: ""
+    phone: "18720587332",
+    password: "18720587332"
   },
   // 用一个函数处理两个数据的收集
   handlePhoneAndPassword(event) {
@@ -41,11 +44,103 @@ Page({
     const passwordInput = event.detail.value;
     console.log(passwordInput);
   },
+  // 点击登录
+  submit() {
+    /**
+     * 1.发送请求
+     *    1.使用封装好的函数
+     * 2.登录成功后返回到个人中心页面，需要的数据有
+     *    1.头像
+     *    2.用户名
+     *    3.听歌历史
+     *  并成功渲染到页面上
+     * 3.把数据保存在storage中
+     * 4.校验
+     * 5.发送请求
+     *  
+     */
+    const {phone, password} = this.data;
+    if(!phone){
+      /**
+       * 1.如果号码为空，进行校验
+       * 2.显示的图标
+       */
+      wx.showToast({
+        title: '号码为空',
+        icon: "none"
+      })
+      return;
+    }
+    // 手机号正则
+    const phoneReg = /^[1]([3-9])[0-9]{9}$/;
+    if(!phoneReg.test(phone)){
+      wx.showToast({
+        title: '手机号格式错误',
+        icon: "none"
+      })
+      return;
+    }
+    if (!password){
+      wx.showToast({
+        title: '密码为空',
+        icon: "none"
+      })
+      return;
+    }
+    // 密码正则
+    const passwordReg = /^[\w]{6,12}$/;
+    if (!passwordReg.test(password)) {
+      wx.showToast({
+        title: '密码格式错误',
+        icon: "none"
+      })
+      return;
+    }
+
+
+    // const data = {
+    //   phone: this.data.phone,
+    //   password: this.data.password
+    // }
+    const promiseLogin = ajax("login/cellphone", this.data);
+    // console.log(promiseLogin);
+    promiseLogin.then((res) => {
+      /**
+       * 1.跳转
+       *    1.定义事件
+       *    2.在成功的回调中传送数据
+       * 2.把数据保存在storage中再跳转
+       *    1.调用api保存数据到storage
+       *    2.数据返回成功
+       *    3.保存成功之后进行跳转
+       */
+      if(res.data.code === 200){
+        /**
+         * 1.功能成功
+         * 2.保存数据到storage中
+         */
+        wx.setStorage({
+          key: 'userInfo',
+          data: JSON.stringify(res.data.profile)
+        })
+      }else{
+        wx.showToast({
+          title: '登录失败',
+          icon:"none"
+        })
+        return;
+      }
+      console.log(res);
+      wx.switchTab({
+        url: '../personal/personal'
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,8 +1,11 @@
+import ajax from "../../utils/ajax.js";
 // pages/personal/personal.js
 /**
  * 1.拖拽
  *    1.给元素绑定事件
  * 2.登录页面
+ * 3.请求历史记录的数据
+ *    1.静态页面
  */
 Page({
 
@@ -13,6 +16,12 @@ Page({
     // 拖拽块的坐标
     moveY: 0,
     isTransition: "",
+    //头像
+    avatarUrl:"",
+    // 用户名
+    username: "",
+    // 历史记录
+    historyList: []
   },
   // 给可拖拽元素绑定手指触摸事件
   handleTouchStart(event) {
@@ -67,7 +76,41 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    /**
+     * 1.登录成功跳转到这个页面，这次的页面加载后拿到数据进行渲染
+     *    1.读取到storage中的数据
+     * 2.没有登录和登录之后是两个状态
+     *    1.登录之后会有一个token作为登录成功的标识
+     *    2.并不是说登录成功就用了另一个系统了，而是说我登录成功之后就会得到数据，拿到这些数据来对页面进行渲染
+     *    3.很多东西就是需要成功之后才会渲染的，是因为登录成功之后拿到了数据
+     *    4.token只是登录成功的标识而已，它也只是一个数据而已，可以通过这个token来标识当前用户，在当前用户下做一些事
+     * 3.需要渲染头像
+     * 4.历史记录是需要拿到uid再进行一次请求的
+     * 5.拿到数据再去渲染
+     */
+    wx.getStorage({
+      key: 'userInfo',
+      success: async (res)=>{
+        const { avatarUrl, nickname, userId } = JSON.parse(res.data);
+        console.log(res.data, avatarUrl, nickname, userId);
+        this.setData({
+          avatarUrl,
+          nickname
+        })
+        /**
+         * 1.拿到userId后去请求历史数据
+         * 2.拿到数据后对数据进行处理拿到有用的数据
+         */
+        const historyAjax = await ajax("user/record", {uid:userId,type: 1});
+        const historyUseful = historyAjax.data.weekData.map((history) => {
+          return history.song.al.picUrl;
+        })
+        this.setData({
+          historyList: historyUseful
+        })
+        console.log(historyUseful);
+      },
+    })
   },
 
   /**
